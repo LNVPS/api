ec54713593f633d799553ae75a05992108f89dcf
kieran <kieran@harkin.me>
2025-02-21 11:01:58 +0000

    chore: misc config changes

--- dev_setup.sql
+++ dev_setup.sql
@@ -5,7 +5,7 @@ ignore into vm_host(id,kind,region_id,name,ip,cpu,memory,enabled,api_token)
 values(1, 0, 1, "lab", "https://185.18.221.8:8006", 4, 4096*1024, 1, "root@pam!tester=c82f8a57-f876-4ca4-8610-c086d8d9d51c");
 insert
 ignore into vm_host_disk(id,host_id,name,size,kind,interface,enabled)
-values(1,1,"local-lvm",1000*1000*1000*1000, 0, 0, 1);
+values(1,1,"local-zfs",1000*1000*1000*1000, 0, 0, 1);
 insert
 ignore into vm_os_image(id,distribution,flavour,version,enabled,url,release_date)
 values(1, 0,"Server","24.04",1,"https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img","2024-04-25");
